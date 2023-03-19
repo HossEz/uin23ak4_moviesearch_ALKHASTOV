53d0752a505fb241b9024150f3228a2cf4da9b37
HossEz <41141267+HossEz@users.noreply.github.com>
2023-03-20 00:55:03 +0100

    endringer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import MovieCard from './components/MovieCard';
-import MovieList from './components/MovieList';
-import './sass/main.scss';
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Layout from "./components/Layout";
+import SearchResults from "./components/SearchResults";
+import MovieList from "./components/MovieList";
+import './css/main.css'
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [NoResults, setNoResults] = useState(false);
+
+  const handleSearch = (searchQuery) => {
+    setQuery(searchQuery);
+  };
+
   return (
-    <MovieList />
-  )
+    <Layout query={query}>
+      <SearchResults handleSearch={handleSearch} />
+      <MovieList query={query} setNoResults={setNoResults} NoResults={NoResults} />
+    </Layout>
+  );
 }
 
 export default App;
