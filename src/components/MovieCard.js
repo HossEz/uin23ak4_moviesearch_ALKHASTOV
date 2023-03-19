40658c74aa87861010177777d31ee26551eb1bd2
HossEz <41141267+HossEz@users.noreply.github.com>
2023-03-20 00:59:33 +0100

    endringer

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,16 +1,15 @@
 export default function MovieCard({ movie }) {
-    const { Title, Year, imdbID, Type, Poster } = movie;
-  
-    return (
-      <article className="movie-card">
-        <img src={Poster} alt={Title} />
-        <h3>{Title}</h3>
-        <p>{Year}</p>
-        <p>{Type}</p>
-        <a href={`https://www.imdb.com/title/${imdbID}`} target="_blank" rel="noopener noreferrer">
-          More info
-        </a>
-      </article>
-    );
-  }
-  
+  const { Title, Year, imdbID, Type, Poster } = movie;
+
+  return (
+    <article className="movie-card">
+      <img src={Poster} alt={Title} />
+      <h3>{Title}</h3>
+      <p>År: {Year}</p>
+      <p>Type: {Type}</p>
+      <a href={`https://www.imdb.com/title/${imdbID}`} target="_blank" rel="noopener noreferrer">
+        Les mer
+      </a>
+    </article>
+  );
+}
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,15 +1,55 @@
+import { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
 
+export default function MovieList({ query, setNoResults, NoResults }) {
+  const [movies, setMovies] = useState([]);
 
-export default function MovieList({ movies }) {
-    console.log("movies:", movies);
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const trimQuery = query.trim();
+      if (trimQuery) {
+        const response = await fetch(`https://www.omdbapi.com/?apikey=74541327&s=${trimQuery}&type=movie`);
+        const data = await response.json();
+        if (data && data.Search) {
+          setMovies(data.Search);
+          setNoResults(false);
+        } else {
+          setNoResults(true);
+        }
+      }
+    };
+
+    fetchMovies();
+  }, [query, setNoResults]);
+
+
+  useEffect(() => {
+    const fetchMoviesJB = async () => {
+      const response = await fetch(`https://www.omdbapi.com/?apikey=74541327&s=James+Bond&type=movie`);
+      const data = await response.json();
+      setMovies(data.Search);
+    };
+
+    if (movies.length === 0) {
+      fetchMoviesJB();
+    }
+  }, [movies]);
+
+
+    // console.log(movies.length);
+    // console.log(movies);
 
   return (
-    <div className="movie-list">
-      {movies?.map((movie) => (
-        <MovieCard key={movie.imdbID} movie={movie} />
-      ))}
-    </div>
+    <>
+      {NoResults ? (
+        <p className="noResults">Ingen resultater funnet.</p>
+      ) : (
+        <div className="movie-list">
+          {movies?.map((movie) => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))}
+        </div>
+      )}
+    </>
   );
 }
-
